dc42042dfbb8ca8b7ab608f61a7b7b6abbc1f469
barraemme@gmail.com <barraemme@gmail.com@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-02-16 17:17:35 +0000

    come da specifica tecnica l'algoritmo verrà inplementato come modulo.. trovate il file in libs/algorithm.rb. al momento il metodo stampa una stringa. lo schedulatore chiama il metodo del modulo
    git-svn-id: http://subversion.assembla.com/svn/sigeol@167 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -3,17 +3,25 @@
 require 'rubygems'
 require 'rufus/scheduler'
 
+include Algorithm
+
 ActiveRecord::Base.allow_concurrency = true
 
 scheduler ||= Rufus::Scheduler.start_new
+puts "Scheduler in esecuzione.."
 
 #test
 scheduler.every("10s") do
-  puts "puttanate ogni 10s  "
+  do_algorithm
+  ActiveRecord::Base.verify_active_connections! 
 end
 
 #da sostituire con (la data di esecuzione andrà presa dal database o da un file)
-#@job_id = @scheduler.at "Sun Oct 07 14:24:01 +0900 2009" do
-#  do_something()
+#@job_id = @scheduler.at "Sun Oct 07 14:24:01 +0900 2009" :tags => [ "algorithm", "important" ] do
+#  do_something
 #  ActiveRecord::Base.verify_active_connections!  
 #end
+
+def scheduler.log_exception (e)
+  ActiveRecord::Base.logger.warn("Si è verificato un problema: ", e)
+end
