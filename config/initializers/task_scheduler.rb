ebe3cf184838d7dfd48c4188d79951a5329d07af
barraemme@gmail.com <barraemme@gmail.com@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-02-17 17:03:16 +0000

    modifiche strutturali e test sull'avvio di una funzione in un particolare orario->OK
    git-svn-id: http://subversion.assembla.com/svn/sigeol@171 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,27 +1,54 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
+#QuiXoft - Progetto ”SIGEOL”
+#NOME FILE: task_scheduler.rb
+#VERSIONE: 0.2.0
+#AUTORE: Barbiero Mattia
+#DATA CREAZIONE: 14/02/2009
+#REGISTRO DELLE MODIFICHE:
+#17/02/09 Aggiunta cron style e log exception
+#16/02/09 Prima stesura
 require 'rubygems'
 require 'rufus/scheduler'
 
 include Algorithm
 
-ActiveRecord::Base.allow_concurrency = true
-
-scheduler ||= Rufus::Scheduler.start_new
+#inizializzazione dell'algoritmo
+@scheduler ||= Rufus::Scheduler.start_new
 puts "Scheduler in esecuzione.."
 
-#test
-scheduler.every("10s") do
-  do_algorithm
-  ActiveRecord::Base.verify_active_connections! 
+
+
+def exec_cron(cron_string)
+  @scheduler.schedule cron_string, :tags => [ "algorithm", "important" ] do
+    do_algorithm
+    ActiveRecord::Base.verify_active_connections!
+  end
 end
 
-#da sostituire con (la data di esecuzione andrà presa dal database o da un file)
-#@job_id = @scheduler.at "Sun Oct 07 14:24:01 +0900 2009" :tags => [ "algorithm", "important" ] do
-#  do_something
-#  ActiveRecord::Base.verify_active_connections!  
-#end
+def execute_in(max_time)
+  @scheduler.schedule_in max_time, :tags => [ "algorithm", "important" ] do
+    do_algorithm
+    ActiveRecord::Base.verify_active_connections!
+ end
+end
 
-def scheduler.log_exception (e)
+
+#utilizziamo la notazione stile cron
+#* * * * *
+#- - - - -
+#| | | | |
+#| | | | ----- giorno della settimana (0 - 7) (domenica = 0 oppure 7)
+#| | | ------- mese (1 - 12)
+#| | --------- giorno del mese (1 - 31)
+#| ----------- ora (0 - 23)
+#------------- minuti (0 - 59)
+
+
+
+def @scheduler.log_exception (e)
   ActiveRecord::Base.logger.warn("Si è verificato un problema: ", e)
 end
+
+#esegue l'algoritmo in base alla stringa cron
+exec_cron("53 17 * * *")
+
+@scheduler.join
