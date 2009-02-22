d8ad57013dcbf4054f0302077feab386b57418b9
ale7forever@gmail.com <ale7forever@gmail.com@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-02-22 22:10:11 +0000

    regexp della mail piu elegante
    git-svn-id: http://subversion.assembla.com/svn/sigeol@197 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,7 +43,7 @@ class User < ActiveRecord::Base
 
   #la mail deve essere del tipo account@qualcosa.qualcosa.it oppure account@qualcosa.qualcosa.it
   validates_format_of :mail,
-                     :with => /^([^@ t]{8,12})+@(([^@ t]{1,12})+\.+[a-z]{2,3})|(([^@ t]{1,12})+\.+([^@ t]{1,12})+\.+[a-z]{2,3})$/,
+                     :with => /^([a-zA-Z0-9_\.\-\+]){8,12}\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/,
                      :message=>"La mail non è valida"
 
   validates_presence_of :random,
