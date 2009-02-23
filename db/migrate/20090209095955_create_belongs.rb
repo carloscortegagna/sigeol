c69e2ac2cf1c0482bfb8ab4f9450d5f269467749
ale7forever@gmail.com <ale7forever@gmail.com@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-02-23 12:37:34 +0000

    modifica alla migratione create_belong, questa è una tabella join ma rails vuole la kiave primaria. Info: http://blog.hasmanythrough.com/2006/4/20/many-to-many-dance-off
    git-svn-id: http://subversion.assembla.com/svn/sigeol@202 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- db/migrate/20090209095955_create_belongs.rb
+++ db/migrate/20090209095955_create_belongs.rb
@@ -1,6 +1,6 @@
 class CreateBelongs < ActiveRecord::Migration
   def self.up
-    create_table :belongs, :id=>false do |t|
+    create_table :belongs do |t|
       t.integer :teaching_id
       t.integer :curriculum_id
       t.boolean :isOptional
