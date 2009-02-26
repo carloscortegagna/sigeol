5f95d0219a057444f033c5abfd76a5156def8d98
ale7forever@gmail.com <ale7forever@gmail.com@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-02-26 12:45:52 +0000

    modifiche al curriculum e graduate_course
    git-svn-id: http://subversion.assembla.com/svn/sigeol@216 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- app/models/curriculum.rb
+++ app/models/curriculum.rb
@@ -7,22 +7,38 @@
 #14/02/09 Aggiunta delle prime validazioni
 #12/02/09 Prima stesura
 class Curriculum < ActiveRecord::Base
- include ApplicationHelper
+  include ApplicationHelper
+
  belongs_to :graduate_course
- has_many :belongs,:dependent=> :destroy
- has_many :teachings, :through=>:belongs
+ has_many :belongs,:dependent => :destroy
+ has_many :teachings, :through =>:belongs
 
   #validazioni :name
   validates_presence_of :name,
-                         :message=>"Il nome non deve essere vuoto"
+                        :message => "Il nome non deve essere vuoto"
   validates_length_of :name,
-                       :maximum=> 50,
-                       :message=>"Il nome è troppo lungo"
+                      :maximum => 50,
+                      :message => "Il nome è troppo lungo"
   validates_format_of :name,
-                     :with => /^[a-zA-Zàòèéùì\s]*$/,
-                     :message=>"Si accetta solo caratteri"
-   #funzione di callback,mette tutto in minuscolo del nome, tranne la prima lettera
+                      :with => /^[a-zA-Zàòèéùì\s]*$/,
+                      :message =>"Si accetta solo caratteri"
+  validates_presence_of :graduate_course_id,
+                        :message => "Deve essere associato ad un corso di laurea"
+
+  #funzione di callback,mette tutto in minuscolo del nome, tranne la prima lettera
  def before_save
    self.name=first_upper(self.name)
  end
+
+#validazioni unicità curriculum-graduate_course
+validate :unique_curriculum_graduate_course?
+
+  private
+  def unique_curriculum_graduate_course?
+    name=first_upper(self.name)
+    if Curriculum.find_by_name_and_graduate_course_id(name,self.graduate_course_id)
+      errors.add_to_base("E' gia presente un curriculum con questo nome")
+    end
+  end
+
 end
--- app/models/graduate_course.rb
+++ app/models/graduate_course.rb
@@ -9,28 +9,31 @@
 
 class GraduateCourse < ActiveRecord::Base
   include ApplicationHelper
-  has_many :expiry_dates, :dependent=>:destroy
+
+  has_many :expiry_dates, :dependent => :destroy
   belongs_to :academic_organization
-  has_many :timetables, :dependent=> :destroy
-  has_many :curriculums, :dependent=>:destroy
+  has_many :timetables, :dependent => :destroy
+  has_many :curriculums, :dependent => :destroy
   has_and_belongs_to_many :users, :uniq => true
   has_and_belongs_to_many :classrooms, :uniq => true
   has_many_polymorphs :constraints, :from => [:quantity_constraints,:boolean_constraints, :temporal_constraints],
-    :as=> :owner, :dependent=> :destroy
+    :as => :owner, :dependent => :destroy
 
   #validazioni :name
   validates_uniqueness_of :name,
                           :message=>"Il corso è già presente"
-
   validates_presence_of :name,
-                         :message=>"Il nome non deve essere vuoto"
-   validates_length_of :name,
+                        :message=>"Il nome non deve essere vuoto"
+  validates_length_of :name,
                        :maximum=> 50,
                        :message=>"Il nome è troppo lungo"
-   validates_format_of :name,
-                     :with => /^[a-zA-Zàòèéùì\s]*$/,
-                     :message=>"Si accetta solo caratteri"
-#funzione di callback,mette tutto in minuscolo del nome, tranne la prima lettera
+  validates_format_of :name,
+                       :with => /^[a-zA-Zàòèéùì\s]*$/,
+                       :message => "Si accetta solo caratteri"
+  validates_presence_of :academic_organization_id,
+                         :message => "Deve essere associata ad un'organizzazione accademica"
+
+  #funzione di callback,mette tutto in minuscolo del nome, tranne la prima lettera
   def before_save
    self.name=first_upper(self.name)
   end
@@ -38,10 +41,10 @@ class GraduateCourse < ActiveRecord::Base
 
   #validazioni :duration
   validates_numericality_of :duration,
-                           :only_integer =>true,
-                           :grater_than_or_euqual_to =>1,
-                           :less_than_or_equal_to =>6,
-                           :message=>"attenzione il numero deve essere compreso tra 1 e 6"
+                            :only_integer =>true,
+                            :grater_than_or_euqual_to =>1,
+                            :less_than_or_equal_to =>6,
+                            :message=>"Attenzione il numero deve essere compreso tra 1 e 6"
 
   validates_presence_of  :duration,
                          :message=>"La durata non deve essere vuota"
