60aa21288fb91101110bd2840215921a82048196
ale7forever@gmail.com <ale7forever@gmail.com@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-02-27 15:27:47 +0000

    integrity constraint aggiornata
    git-svn-id: http://subversion.assembla.com/svn/sigeol@225 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- db/migrate/20090220091637_integrity_constraints.rb
+++ db/migrate/20090220091637_integrity_constraints.rb
@@ -2,14 +2,10 @@ class IntegrityConstraints < ActiveRecord::Migration
   extend MigrationHelper  # see lib/migration_helpers.rb
   def self.up
     fk :teachings, :period_id, :periods
-    #vincoli sulla tabella buildings
-    fk :buildings,:address_id,:addresses
     #vincoli sulla tabella expiry_dates
     fk :expiry_dates,:graduate_course_id,:graduate_courses
     #vincoli sulla tabella graduate_courses
     fk :graduate_courses,:academic_organization_id,:academic_organizations
-    #vincoli sulla tabella users
-    fk :users,:address_id,:addresses
    #vincoli sulla tabella belongs
     fk :belongs,:curriculum_id,:curriculums
     fk :belongs,:teaching_id,:teachings
@@ -25,23 +21,22 @@ class IntegrityConstraints < ActiveRecord::Migration
     fk :timetable_entries,:classroom_id,:classrooms
     fk :timetable_entries,:teaching_id,:teachings
     #vincoli sulla tabella capabilities_users
-    fk :capabilities_users,:user_id,:users
-    fk :capabilities_users,:capability_id,:capabilities
+    fk :capabilities_users,:user_id,:users,2
+    fk :capabilities_users,:capability_id,:capabilities,2
+     #vincoli sulla tabella buildings
+    fk :buildings,:address_id,:addresses,1
+     #vincoli sulla tabella users
+    fk :users,:address_id,:addresses,1
+    fk :teachings, :teacher_id, :teachers,1
+
   end
 
   def self.down
     drop_fk :teachings, :period_id
-    drop_fk :buildings,:address_id
-    #vincoli sulla tabella adresses
     #vincoli sulla tabella expiry_dates
     drop_fk :expiry_dates,:graduate_course_id
     #vincoli sulla tabella graduate_courses
     drop_fk :graduate_courses,:academic_organization_id
-    #vincoli sulla tabella academic_organizations
-    #vincoli sulla tabella teachers
-    #vincoli sulla tabella users
-    drop_fk :users,:address_id
-    #vincoli sulla tabella capabilities
     #vincoli sulla tabella belongs
     drop_fk :belongs,:curriculum_id
     drop_fk :belongs,:teaching_id
@@ -59,6 +54,10 @@ class IntegrityConstraints < ActiveRecord::Migration
     #vincoli sulla tabella capabilities_users
     drop_fk :capabilities_users,:user_id
     drop_fk :capabilities_users,:capability_id
+    drop_fk :buildings,:address_id
+    #vincoli sulla tabella users
+    drop_fk :users,:address_id
+    drop_fk :teachings,:teacher_id
 
   end
 end
