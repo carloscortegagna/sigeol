ec905274f2ec8b5211645170f99f2f6e7be8ecdb
ale7forever@gmail.com <ale7forever@gmail.com@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-02-27 15:28:05 +0000

    migration helper aggiornato
    git-svn-id: http://subversion.assembla.com/svn/sigeol@226 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- lib/migration_helper.rb
+++ lib/migration_helper.rb
@@ -1,12 +1,19 @@
  module MigrationHelper
 
- def fk(from_table, from_column, to_table)
-  execute %(alter table #{from_table}
+ def fk(from_table, from_column, to_table,del=0)
+  c= %(alter table #{from_table}
             add constraint #{constraint_name(from_table, from_column)}
             foreign key (#{from_column})
             references #{to_table}(id)
-
             )
+  case del
+  when 1
+    c=c+" ON DELETE SET NULL"
+  when 2
+    c=c+" ON DELETE CASCADE"
+  end
+  execute c
+
 end
 
  def drop_fk(from_table, from_column)
