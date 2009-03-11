98a2163749a1588828ee11582e2286c72e104f47
ale7forever@gmail.com <ale7forever@gmail.com@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-03-11 07:32:19 +0000

    building_test
    git-svn-id: http://subversion.assembla.com/svn/sigeol@342 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- test/unit/building_test.rb
+++ test/unit/building_test.rb
@@ -1,8 +1,26 @@
 require 'test_helper'
 
 class BuildingTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+ def setup
+   @b=Building.new
+ end
+
+ def test_attribute_not_nil
+    assert !@b.valid
+    assert @b.error.invalid?(:name)
+    assert @b.error.invalid?(:address_id)
+ end
+ 
+ def test_unique_name
+    @b.name=Bulding(:buildings_1).name
+    @b.address=Bulding(:buildings_1).address
+    assert !@b.save
+    assert_equal "Esiste già un palazzo con questo nome", @b.errors.on(:name)
+ end
+
+ def test_destroy_building
+   b=Building(:buildings_1)
+   assert @b.destroy
+   assert !Classroom.find(:all)
+ end
 end
