35599b4589cfb456efa3fd7ceb25f156843bec52
ale7forever@gmail.com <ale7forever@gmail.com@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-05-26 13:43:17 +0000

    non erano gestiti i seguenti casi: creazione di un classe con 0 palazzi; creazione di un curriculum con 0 corsi di laurea, creazione di un insegnamento con 0 docenti e 0 curricula!!
    git-svn-id: http://subversion.assembla.com/svn/sigeol@525 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -31,11 +31,11 @@ class Teacher < ActiveRecord::Base
 
   #validazioni attributo :name e :surname
   validates_presence_of :name, :surname,
-                        :message=>"Il nome non deve essere vuoto",
+                        :message=>"Il campo non deve essere vuoto",
                         :on => :update
   validates_length_of :name,:surname,
                       :maximum=> 30,
-                      :message=>"Il nome è troppo lungo",
+                      :message=>"Il campo è troppo lungo",
                       :on => :update
   validates_format_of :name,:surname,
                       :with => /^[a-zA-Zàòèéùì\s]*$/,
